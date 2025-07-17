aa5db0ab55701997674a9940d2f57ac8122d9ee9
Aryan Kalra <aryan.smart832@gmail.com>
2025-07-17 12:15:47 +0530

    implementing socket in front and back

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,64 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
-  return <div className="bg-red-400 w-full h-dvh">hello</div>;
+  const socketRef = useRef<WebSocket>(null);
+  const [messages, setMessages] = useState<Array<string | WebSocket>>([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    socketRef.current = new WebSocket("ws://localhost:8000/ws");
+    socketRef.current.onopen = (event: Event) => {
+      console.log("WebSocket connection established");
+
+      // console.log("event.data=>", event.data);
+      // setMessages((prev)=>[...prev,event.data])
+    };
+    socketRef.current.onmessage = (event: MessageEvent<WebSocket>) => {
+      console.log("event.data=>", event.data);
+      setMessages((prev) => [...prev, event.data]);
+    };
+
+    socketRef.current.onclose = () => {
+      console.log("Websocket closed");
+    };
+
+    return () => {
+      socketRef.current?.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socketRef.current && input.trim()) {
+      socketRef.current.send(input);
+      setInput("");
+    }
+  };
+  return (
+    <div className="w-full h-dvh">
+      <div className="p-2 flex flex-col ">
+        <h1 className="text-2xl">Fast Api Websocket Chats</h1>
+        <div className="flex flex-col gap-y-2">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            value={input}
+            className="border-[1px] w-1/2"
+          />
+          <button
+            onClick={sendMessage}
+            className="hover:bg-blue-600 cursor-pointer rounded-md w-fit p-2 text-black border-[1px] border-slate-400 hover:text-white"
+          >
+            Send
+          </button>
+        </div>
+        <div>
+          {messages?.map((message: string | any, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- server/web-socket/main.py
+++ server/web-socket/main.py
@@ -1,5 +1,37 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
+# from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def healthz():
+    return {"message": "Healthy..."}
+
+
+active_connections = []
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    # active_connections.append(websocket)
+
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text is {data}")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
