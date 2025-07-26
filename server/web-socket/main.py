830a5c75eb05c7d7a8f7e5ae261ba0612ebc78d3
Aryan Kalra <aryan.smart832@gmail.com>
2025-07-26 14:59:31 +0530

    rm cl

--- server/web-socket/main.py
+++ server/web-socket/main.py
@@ -22,33 +22,6 @@ def healthz():
     return {"message": "Healthy..."}
 
 
-active_connections = []
-
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     # active_connections.append(websocket)
-
-#     while True:
-#         data = await websocket.receive_text()
-#         await websocket.send_text(f"Message text is {data}")
-
-
-# @app.websocket("/ws/{client_id}")
-# async def websocket_endpoint(websocket: WebSocket, client_id: int):
-#     await manager.connect(websocket, client_id)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             # await manager.send_personal_message(f"You wrote: {data}", websocket)
-#             await manager.broadcast(data, client_id)
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket, client_id)
-#         await manager.broadcast(f"Client Id {client_id} left the chat.", client_id)
-
-
-# rooms: Dict[str, List[WebSocket]] = {}
 @app.websocket("/ws/message/{room}")
 async def track_cursor(websocket: WebSocket, room: int):
     # await websocket.accept()
@@ -65,13 +38,6 @@ async def track_cursor(websocket: WebSocket, room: int):
     except Exception as error:
         print(f"\nSomething went wrong {websocket} \n", error)
         user.disconnect(websocket, room, name)
-        cursorMovement.disconnect(websocket, room, name)
-        await stickerMovement.broadcast(
-            {"message": f"Client Id {name} left the chat.", "name": name},
-            room,
-            websocket,
-            name,
-        )
 
 
 @app.websocket("/ws/cursor/{room}")
@@ -88,14 +54,8 @@ async def track_cursor(websocket: WebSocket, room: int):
 
     except Exception as error:
         print(f"\nSomething went wrong {websocket} \n", error)
-        user.disconnect(websocket, room, name)
+
         cursorMovement.disconnect(websocket, room, name)
-        await stickerMovement.broadcast(
-            {"message": f"Client Id {name} left the chat.", "name": name},
-            room,
-            websocket,
-            name,
-        )
 
 
 @app.websocket("/ws/remove/{room}")
@@ -115,7 +75,6 @@ async def track_cursor(websocket: WebSocket, room: int):
         print(f"\nSomething went wrong {websocket} \n", error)
         print("Remove Socket Error\n ", error)
 
-        user.disconnect(websocket, room, name)
         stickerMovement.disconnect(websocket, room, name)
         await stickerMovement.broadcast(
             {"message": f"Client Id {name} left the chat.", "name": name},
